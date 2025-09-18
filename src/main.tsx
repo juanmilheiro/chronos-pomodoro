9d0543a91926bb21234f295020ab0474afd003ee
Juan Milheiro <juancerveiramilheiro@gmail.com>
2025-09-18 11:37:51 -0300

    Adicionando o prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import QualquerCoisa from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QualquerCoisa />
+    return (<div>
+        <h1>Hello World!</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet est temporibus eos ullam expedita in, alias nemo impedit rerum incidunt totam quam, aut numquam ipsam nisi soluta sint praesentium veniam?
+        </p>
+    </div>)
   </StrictMode>,
 )
