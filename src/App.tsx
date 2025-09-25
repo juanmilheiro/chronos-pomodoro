410890f1e9aed7287b02d0dd5e65842c71539b16
Juan Milheiro <juancerveiramilheiro@gmail.com>
2025-09-25 10:31:00 -0300

    Mudei a posição do export

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-function App() {
-    console.log('Hello.');
+export function App() {
+  console.log('Hello.');
 
-    return <h1>Hello World (do App)!</h1>;
-}
-
-export default App;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos sed nam ea
+        illum praesentium fugit eum minus a possimus harum ipsum inventore
+        temporibus optio voluptatem, nulla asperiores consectetur expedita.
+        Cupiditate!
+      </p>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import QualquerCoisa from './App'
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QualquerCoisa />
-    return (<div>
-        <h1>Hello World!</h1>
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet est temporibus eos ullam expedita in, alias nemo impedit rerum incidunt totam quam, aut numquam ipsam nisi soluta sint praesentium veniam?
-        </p>
-    </div>)
+    <App/>
   </StrictMode>,
 )
