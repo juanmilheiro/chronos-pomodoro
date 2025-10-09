075655ff9ad935da77b9416d4b17027be7970a6a
Juan Milheiro <juancerveiramilheiro@gmail.com>
2025-10-09 16:49:45 -0300

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
@@ -5,14 +7,14 @@ export function App() {
   console.log('Hello.');
 
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <>
+      <Heading/>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos sed nam ea
         illum praesentium fugit eum minus a possimus harum ipsum inventore
         temporibus optio voluptatem, nulla asperiores consectetur expedita.
         Cupiditate!
       </p>
-    </div>
+    </>
   );
 };
